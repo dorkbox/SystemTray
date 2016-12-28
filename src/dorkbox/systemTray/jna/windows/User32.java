c12eacf3fcb0ee48ce0fcad89aaab50e3c81be80
nathan <nathan@dorkbox.com>
2016-12-28 23:48:06 +0100

    Fixed package name (is now lowercase)

--- src/dorkbox/systemTray/jna/windows/User32.java
+++ src/dorkbox/systemTray/jna/windows/User32.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dorkbox.systemTray.jna.Windows;
+package dorkbox.systemTray.jna.windows;
 
 import com.sun.jna.Pointer;
 
