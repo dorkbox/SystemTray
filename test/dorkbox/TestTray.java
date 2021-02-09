dc2b4aa8e0f80b36b84693572e424610aa58ca3e
Robinson <robinson@dorkbox.com>
2021-02-10 00:47:37 +0100

    back to auto-detect type

--- test/dorkbox/TestTray.java
+++ test/dorkbox/TestTray.java
@@ -60,7 +60,7 @@ class TestTray {
     public
     TestTray() {
         SystemTray.DEBUG = true; // for test apps, we always want to run in debug mode
-        SystemTray.FORCE_TRAY_TYPE = SystemTray.TrayType.Swing;
+        // SystemTray.FORCE_TRAY_TYPE = SystemTray.TrayType.Swing;
 
         // for test apps, make sure the cache is always reset. These are the ones used, and you should never do this in production.
         CacheUtil.clear("SysTrayExample");
