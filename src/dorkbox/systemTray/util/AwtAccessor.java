3eb7edfebac22b75dfca391a7b932f528c04bcbf
Robinson <robinson@dorkbox.com>
2023-01-03 00:09:54 +0100

    Added copyright/comments

--- src/dorkbox/systemTray/util/AwtAccessor.java
+++ src/dorkbox/systemTray/util/AwtAccessor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 dorkbox, llc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dorkbox.systemTray.util;
 
 import java.awt.Component;
@@ -6,6 +21,7 @@ import java.awt.Menu;
 
 public
 class AwtAccessor {
+    // the class methods here are rewritten using javassist.
     public static Object getPeer(java.awt.MenuComponent nativeComp) {
         return null;
     }
