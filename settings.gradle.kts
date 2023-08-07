89c2865659f7278ae4dbd847279e953a8de392e7
Robinson <robinson@dorkbox.com>
2023-08-07 17:24:51 -0600

    Hardcoded project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 dorkbox, llc
+ * Copyright 2023 dorkbox, llc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,3 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+rootProject.name = "SystemTray"
